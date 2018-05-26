92d4e90003d1b95675c166f324d7ddf8bd61dca9
Arto Bendiken <arto@dryproject.org>
2018-05-26 15:08:18 +0300

    Added a build dependency on the sphinx-polyglot extension.
    See: https://github.com/bendiken/sphinx-polyglot

--- conf.py
+++ conf.py
@@ -11,10 +11,12 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+sys.path.append(os.path.abspath('.extensions'))
+sys.path.append(os.path.abspath('.extensions/sphinx-polyglot'))
+
 import sphinx_bootstrap_theme
 
 # -- Project information -----------------------------------------------------
@@ -31,6 +33,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
+    'sphinx-polyglot', # file:.extensions/sphinx-polyglot/sphinx-polyglot.py
 ]
 templates_path = ['.templates']
 source_suffix = '.rst'
